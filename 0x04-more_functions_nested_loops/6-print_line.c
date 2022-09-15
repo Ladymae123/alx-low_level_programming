fab8dde810972c211268ae7a88b9662a29a41d49
Nneoma Ubaka <ofubann@gmail.com>
2022-09-15 16:29:34 -0700

    Task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,21 +1,14 @@
 #include "main.h"
 
 /**
- * more_numbers - prints 10 times the numbers 0 to 14
+ * print_line - draws a straight line
+ * @n: the length of the line
  */
-void more_numbers(void)
+void print_line(int n)
 {
 	int i;
-	int j;
 
-	for (j = 0; j < 10; j++)
-	{
-		for (i = 0; i <= 14; i++)
-		{
-			if (i > 9)
-				_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
-		}
-		_putchar('\n');
-	}
+	for (i = 1; i <= n; i++)
+		_putchar('_');
+	_putchar('\n');
 }
